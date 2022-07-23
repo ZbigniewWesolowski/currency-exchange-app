89b757bab4288437ee881046f502851680eaf503
admin <zbigniew.wesolowski123@gmail.com>
2022-07-23 22:40:31 +0200

    jpql

--- src/main/java/com/example/currencyexchangeapp/repository/UserRepository.java
+++ src/main/java/com/example/currencyexchangeapp/repository/UserRepository.java
@@ -2,9 +2,17 @@ package com.example.currencyexchangeapp.repository;
 
 import com.example.currencyexchangeapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query(
+            value = "SELECT * FROM USERS u WHERE u.email IS EMPTY",
+            nativeQuery = true)
+    Collection<User> findAllActiveUsersNative();
 
+    //todo zapoznać się z java persistence query langue!!
 }
--- src/main/java/com/example/currencyexchangeapp/view/TransactionViewController.java
+++ src/main/java/com/example/currencyexchangeapp/view/TransactionViewController.java
@@ -1,13 +1,15 @@
 package com.example.currencyexchangeapp.view;
 
+import com.example.currencyexchangeapp.controller.dto.AccountDto;
 import com.example.currencyexchangeapp.controller.dto.CreateTransactionDto;
+import com.example.currencyexchangeapp.controller.dto.CurrencyDto;
 import com.example.currencyexchangeapp.controller.dto.TransactionDto;
+import com.example.currencyexchangeapp.service.AccountService;
 import com.example.currencyexchangeapp.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.util.List;
 
 @Controller
@@ -15,6 +17,7 @@ public class TransactionViewController {
 
     @Autowired
     TransactionService transactionService;
+    AccountService accountService;
 
     @GetMapping("/transaction-table")
     public ModelAndView displayTransactions() {
@@ -27,9 +30,10 @@ public class TransactionViewController {
     @GetMapping("create-transaction")
     public ModelAndView displayCreateTransactionForm() {
         CreateTransactionDto createTransactionDto = new CreateTransactionDto();
+//        List<AccountDto> accountDtoList = accountService.getAllAccounts();
         ModelAndView modelAndView = new ModelAndView("create-transaction");
+//        modelAndView.addObject("accountsListDto", accountDtoList);
         modelAndView.addObject("createDto", createTransactionDto);
         return modelAndView;
     }
-
 }
